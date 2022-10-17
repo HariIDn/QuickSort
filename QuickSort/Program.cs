3d4ab1d26e2ce06b5c6d4ac6dd9f4f4e6caa13ac
Aditya Hari <aditya.hari.ft21@mail.umy.ac.id>
2022-10-17 16:57:03 +0700

    add "read" method

--- QuickSort/Program.cs
+++ QuickSort/Program.cs
@@ -13,6 +13,20 @@ namespace QuickSort
         private int mov_count = 0;
 
         private int n;
+
+        void read()
+        {
+            while (true)
+            {
+                Console.Write("Enter the number of element in the array");
+                string s = Console.ReadLine();
+                n = Int32.Parse(s);
+                if (n <= 20)
+                    break;
+                else
+                    Console.WriteLine("\n Array can have maximum 20 elements\n");
+            }
+        }
         static void Main(string[] args)
         {
         }
